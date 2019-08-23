4ba9a240e9f7090ff5b9674af1b6dc71b9172777
Otavio e Gabriel <otavioabottin@outlook.com>
2019-08-23 20:20:43 -0300

    Exercicio6

---
+++ exe/exercicio6.js
@@ -0,0 +1,4 @@
+var area = function (compri, larg) {
+    return compri * larg;
+};
+console.log(area(2, 8));
---
+++ exercicio6.ts
@@ -0,0 +1,6 @@
+let area = function(compri:number, larg:number):number{
+
+    return compri * larg
+}
+
+console.log(area(2,8))
