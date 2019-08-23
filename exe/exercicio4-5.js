baa505808266fe7018144d897362df14d7ddfdfa
Otavio e Gabriel <otavioabottin@outlook.com>
2019-08-23 20:10:51 -0300

    Exercicio 4

---
+++ exe/exercicio4-5.js
@@ -0,0 +1,9 @@
+var verificarPI = function (num) {
+    if (num % 2 == 0) {
+        console.log("O número é par.");
+    }
+    else {
+        console.log("O número é impar.");
+    }
+};
+verificarPI(8);
---
+++ exercicio4-5.ts
@@ -0,0 +1,11 @@
+let verificarPI = function (num:number):void{
+
+    if(num % 2 == 0){
+        console.log("O número é par.")
+    }
+    else {
+        console.log("O número é impar.")
+    }
+}
+
+verificarPI(8)
